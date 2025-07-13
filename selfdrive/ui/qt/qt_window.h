bf3f07e7a2933bfda1c0e9280e43253b13359cc1
mx <599590980@qq.com>
2025-07-13 21:21:00 +0800

    feat(ui): Adapter ui size to 800x400

--- selfdrive/ui/qt/qt_window.h
+++ selfdrive/ui/qt/qt_window.h
@@ -15,6 +15,6 @@
 #include "system/hardware/hw.h"
 
 const QString ASSET_PATH = ":/";
-const QSize DEVICE_SCREEN_SIZE = {2160, 1080};
+const QSize DEVICE_SCREEN_SIZE = {800, 420};
 
 void setMainWindow(QWidget *w);
